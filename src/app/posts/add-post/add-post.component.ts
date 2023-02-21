fc82fbe5b78d3b95085d5065dfae85df1fc8a848
lazarusmwangim <swanlaz64@gmail.com>
2023-02-21 01:22:26 -0500

    Create Reactive add posts form with validation

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/+store/app.state';
 
@@ -8,10 +9,35 @@ import { AppState } from 'src/app/+store/app.state';
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-  
+  postForm!: FormGroup;
+
   constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.postForm = new FormGroup({
+      title: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      description: new FormControl(null, [Validators.required, Validators.minLength(10)])
+    })
   }
 
+  showDescription() {
+    const desc = this.postForm.get('description');
+    if (desc?.touched && !desc.valid) {
+      if (desc.errors?.['required']) {
+        return 'Description is required.'
+      }
+      if (desc.errors?.['minlength']) {
+        return 'Description length shoukd be at least 10 characters'
+      }
+    }
+
+    return 
+  }
+
+  onAddPost() {
+    if(!this.postForm.valid){
+      return;
+    }
+    console.log(this.postForm.value)
+  }
 }
